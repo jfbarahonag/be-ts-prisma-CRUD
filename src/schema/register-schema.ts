447412bba7cc013ca54b93fe47be534c83fa8ef8
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-10-26 10:31:12 -0500

    messages were added to schema checks

--- src/schema/register-schema.ts
+++ src/schema/register-schema.ts
@@ -1,10 +1,10 @@
 import { body } from "express-validator"
 
 const schema = [
-  body('username').isEmail(),
+  body('username').isEmail().withMessage('username must contain a valid email'),
   body('firstName').exists({checkFalsy: true})
-                    .isLength({min: 1}),
-  body('password').isLength({ min: 5 })
+                    .isLength({min: 2}).withMessage('first nane must be at least 2 characters long'),
+  body('password').isLength({ min: 12 }).withMessage('password must be at least 12 characters long')
 ]
 
 export  { schema as registerSchema }
