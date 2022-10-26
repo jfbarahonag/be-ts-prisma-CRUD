120f3345d9844a7e8ca3ef8baacb2e040801476e
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-10-26 09:31:32 -0500

    server setup and a post request was added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("HOLAAA");
+import express, {Request, Response} from "express";
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+
+// routes
+app.post('/user',
+  (req: Request, res: Response) => {
+    console.log(req.body);
+    return res.json({status: "ok"})
+})
+
+// setup
+const PORT = process.env.PORT || 3000
+app.listen(PORT,  () => {
+  console.log(`Server running on port ${PORT}`)
+  
+})
